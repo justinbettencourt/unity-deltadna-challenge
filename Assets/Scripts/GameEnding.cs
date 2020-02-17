13699fdc27dbcc6c26aad004b4c21f463b604d35
Justin Bettencourt <justinvbettencourt@gmail.com>
2020-02-16 19:41:04 -0500

    Initial Push

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using DeltaDNA;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource caughtAudio;
+
+    bool m_IsPlayerAtExit;
+    public bool m_IsPlayerCaught;
+    float m_Timer;
+    bool m_HasAudioPlayed;
+
+	public GameObject GameManagerObj;
+	public int missionID;
+	public string missionName;
+	// public string levelName;
+
+	void Start() {
+		GameManagerObj = GameObject.Find("GameManager");
+		missionID = GameManagerObj.GetComponent<GameManager>().missionID;
+		missionName = GameManagerObj.GetComponent<GameManager>().missionName;
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    void Update ()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
+        }
+        else if (m_IsPlayerCaught)
+        {
+			EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+            
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+				// Before reloading the scene, capture information for DDNA.
+				DDNA_missionFailedEvent();
+                SceneManager.LoadScene (0);
+            }
+            else
+            {
+				DDNA_missionCompletedEvent();
+                SceneManager.LoadScene (0);
+            }
+        }
+    }
+
+	void DDNA_missionFailedEvent () {
+		GameEvent missionFailedEvent = new GameEvent ("missionFailed")
+			.AddParam("isTutorial", false)
+			.AddParam("missionID", missionID.ToString())
+			.AddParam("missionName", missionName)
+			.AddParam("terminationReason", "Enemy Detection");
+
+		DDNA.Instance.RecordEvent(missionFailedEvent);	
+	}
+
+	void DDNA_missionCompletedEvent () {
+		GameEvent missionCompletedEvent = new GameEvent ("missionCompleted")
+			.AddParam("isTutorial", false)
+			.AddParam("missionID", missionID.ToString())
+			.AddParam("missionName", missionName);
+
+		DDNA.Instance.RecordEvent(missionCompletedEvent);
+		
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DeltaDNA;
+
+public class GameManager : MonoBehaviour {
+
+	public int missionID;
+	public string missionName;
+
+    // Start is called before the first frame update
+    void Start() {
+		// Configure SDK
+		DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
+		DDNA.Instance.StartSDK();
+
+		if (missionID == 0) {
+			missionName = "Demo Mission";
+			Debug.Log(missionName);
+		}
+
+		GameEvent missionStartedEvent = new GameEvent ("missionStarted")
+			.AddParam("isTutorial", false)
+			.AddParam("missionDifficulty", "EASY")
+			.AddParam("missionID", missionID.ToString())
+			.AddParam("missionName", missionName);
+
+		DDNA.Instance.RecordEvent(missionStartedEvent);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
